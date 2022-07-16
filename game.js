0d1b79299f9dfb055bf4fc9d731a6d9ddef90672
Tirth Patel <ptirth631@gmail.com>
2022-07-16 22:16:47 +0530

    Write `playGame()` function for playing game
    Make function playGame() which takes two arguments: playerMove and
    computerMove, and then it determines who is winner. This function is
    part of game logic.
    
    To determine winner it uses the winner the function uses array of the
    moves. It gets the difference of index to find the winner.

--- game.js
+++ game.js
@@ -4,3 +4,21 @@ function computerMove() {
   let number = Math.floor(Math.random() * 3);
   return moves[number]
 }
+
+function playGame(playerMove, computerMove) {
+  let playerMoveIndex = moves.indexOf(playerMove);
+  let computerMoveIndex = moves.indexOf(computerMove);
+
+  let msg;
+  if (playerMoveIndex - computerMoveIndex == 1 ||
+    playerMoveIndex - computerMoveIndex == -2) {
+      msg = `You win! ${playerMove} beats ${computerMove}`;
+  } else if (playerMoveIndex - computerMoveIndex == -1 ||
+    playerMoveIndex - computerMoveIndex == 2) {
+      msg = `You lose! ${computerMove} beats ${playerMove}`;
+  } else {
+    msg = "It's a draw"
+  }
+
+  return msg;
+}
